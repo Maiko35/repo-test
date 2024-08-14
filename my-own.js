3a7342607a4ad5e2887a85b2a68e01d14de419ee
Micheal A <94987382+Maiko35@users.noreply.github.com>
2024-08-14 08:59:07 +0000

    first commit

--- my-own.js
+++ my-own.js
@@ -1,8 +1,15 @@
-let x = 2;
-let y = 4;
-let sum = x + y;
+const express = require('express');
+const path = require('path');
 
-console.log(sum);
 
-let numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
+
+
+
+// let x = 2;
+// let y = 4;
+// let sum = x + y;
+
+// console.log(sum);
+
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
